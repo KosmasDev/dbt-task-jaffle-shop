73434d0461b79f7df3cece218e558d843164f3ed
Kosmas Strakosia <kosmas.strakosia@outlook.com>
2025-06-23 21:54:00 +0000

    updates on stg_orders.sql

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -8,7 +8,6 @@ renamed AS (
     SELECT 
         ID AS ORDER_ID,
         CUSTOMER AS CUSTOMER_ID,
-        -- {{ dbt.date_trunc('day','ordered_at') }} as ordered_at,
         ORDERED_AT AS ORDER_DATE,
         STORE_ID AS LOCATION_ID,
         SUBTOTAL AS PRETAX_PRICE,
